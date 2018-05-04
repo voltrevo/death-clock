ac2d2906df1c2a5e067c9565e740b6886b4db7ff
James Anderson <jamesanderson.contact@gmail.com>
2018-05-04 23:01:31 +1000

    working on styles

--- src/Components/Homepage/Component.tsx
+++ src/Components/Homepage/Component.tsx
@@ -1,11 +1,35 @@
 import * as React from 'react';
 import * as App from '../../App';
-
 import * as State from './State';
 import lifeExpectancy from './lifeExpectancy';
 import { HOMEPAGE_ACTION } from '../../GlobalConstants';
+import styled from '../../Util/StyledComponents';
+import { ThemeProvider } from 'styled-components';
+import Theme from '../../Util/Theme';
 
-import './style.css';
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 80px 1fr;
+`;
+
+const AgeInput = styled.input`
+  grid-column: 2;
+  color: ${p => p.theme.colour.SecondaryLight};
+  height: 60px;
+  font-size: 1.5rem;
+  font-weight: 200;
+  border-radius: 5px;
+  box-sizing: border-box;
+  background-color: ${p => p.theme.colour.PrimaryLight};
+  border: 0;
+  text-align: center;
+  :focus {
+    outline: none;
+  }
+  ::placeholder {
+    color: ${p => p.theme.colour.Secondary};
+  }
+`;
 
 export const Component: React.SFC<{ store: App.Store }> = (
   ({store}) => {
@@ -23,21 +47,26 @@ export const Component: React.SFC<{ store: App.Store }> = (
     const state = store.getState().homepage;
 
     return (
-      <div className="homepage">
-        <header className="header">
-          <h1 className="title">Sample App</h1>
-        </header>
-        <div className="body">
-          <input
-            type="text"
-            value={store.getState().homepage.age}
-            onInput={onTextInput}
-          />
-          <div>
-            You have {lifeExpectancy(state.age) - state.age} years left.
+      <ThemeProvider theme={Theme}>
+        <div className="homepage">
+          <header className="header">
+            <h1 className="title">Sample App</h1>
+          </header>
+          <div className="body">
+            <input
+              type="text"
+              value={store.getState().homepage.age}
+              onInput={onTextInput}
+            />
+            <div>
+              You have {lifeExpectancy(state.age) - state.age} years left.
+            </div>
           </div>
         </div>
-      </div>
+          <Container>
+            <AgeInput placeholder="AGE"/>
+          </Container>
+      </ThemeProvider>
     );
   }
 );
---
+++ src/Util/Theme.ts
@@ -0,0 +1,25 @@
+const Colour = {
+    Primary: '#37474f',
+    PrimaryLight: '#62727b',
+    PrimaryDark: '#102027',
+    Secondary: '#afb42b',
+    SecondaryLight: '#e4e65e',
+    SecondaryDark: '#7c8500',
+    OnPrimary: '#ffffff',
+    OnSecondary: '#102027',
+};
+
+const Theme = {
+    colour: {
+        Primary: Colour.Primary,
+        PrimaryLight: Colour.PrimaryLight,
+        PrimaryDark: Colour.PrimaryDark,
+        Secondary: Colour.Secondary,
+        SecondaryLight: Colour.SecondaryLight,
+        SecondaryDark: Colour.SecondaryDark,
+        OnPrimary: Colour.OnPrimary,
+        OnSecondary: Colour.OnSecondary,
+    },
+};
+
+export default Theme;
